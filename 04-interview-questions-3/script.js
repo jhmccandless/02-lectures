0e98669caf878885452d4f9670c307102b6c87d2
Jason McCandless <jhmccandless1105@gmail.com>
2021-05-01 18:44:59 -0500

    finished up the interview questions from last week

--- 04-interview-questions-3/script.js
+++ 04-interview-questions-3/script.js
@@ -110,26 +110,91 @@ The Collatz Conjecture goes like this:
 Take any number n. If n is even, divide it by 2, if n is odd, multiply it by 3 and add 1. Repeat the process indefinitely, and you’ll eventually reach 1.
 
 Given a num variable, can you print all the numbers in num’s Collatz sequence until 1 is reached? Print the numbers space-separated (and each test case on its own line).
-*/
 
+// answer ///////////////////////////////////////////
 const collatz = (num) => {
-  let str = "";
-  for (let i = 1; i >= num; i++) {
-    if (!(num % 2)) {
-      str.push(num / 2);
-    } else if (!(num % 3)) {
-      str.push(num * 3 + 1);
+  let nums = [num];
+  for (let i = 0; i < nums.length; i++) {
+    if (!(nums[i] % 2)) {
+      nums.push(nums[i] / 2);
+    } else if (nums[i] % 2) {
+      if (nums[i] === 1) {
+        break;
+      } else {
+        nums.push(nums[i] * 3 + 1);
+      }
     }
   }
-  console.log(str);
+  console.log(nums.join(" "));
 };
 
-collatz(5);
+collatz(12);
+*/
 
+/*
+How to remove all duplicates from a given string?
+
+Write a program to remove all the duplicate characters from a given input String, like, if the given String is “Java” then the output should be “Jav”. The second or further occurrence of duplicates should be removed.
+
+
+// answer ///////////////////////////////
+const string = "Java";
+
+const newStr = (str) => [...new Set(str)].join("");
+
+console.log(newStr(string));
+*/
+
+/*
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+
+// answer /////////////////////////////////
+// only works for nums//////////////////////
+const paliCheck = () => {
+  let str = "";
+  let ans = [0];
+  for (let i = 99; i >= 10; i--) {
+    for (let j = 99; j >= 10; j--) {
+      str = i * j + "";
+      if (str === str.split("").reverse().join("") && Number(str) > ans[0]) {
+        ans.push(str);
+        ans.shift();
+        break;
+      }
+    }
+  }
+  return ans;
+};
+
+console.log(paliCheck());
+*/
+/*
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+// answer/////////////////////////////////
+const divisible = (mult) => {
+  let num = mult;
+  for (let i = 1; i <= mult; i++) {
+    if (num % i) {
+      num++;
+      i = 1;
+    }
+  }
+  return num;
+};
+
+console.log(divisible(20));
+*/
+
+/*
 // reversing string words
 
 let string = "Javascript is best";
-/*
+
 // Hard coded
 const wordsArr = [];
 let word = string.slice(0, string.indexOf(" "));
