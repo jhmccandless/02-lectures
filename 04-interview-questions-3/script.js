011e39abde17833c5d741b19e58879842f066d44
Jason McCandless <jhmccandless1105@gmail.com>
2021-05-01 12:43:10 -0500

    working on algors from last week along with arrow/ternary

--- 04-interview-questions-3/script.js
+++ 04-interview-questions-3/script.js
@@ -1,7 +1,134 @@
 "use stric";
+/*
+// first practice example
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+sum(40, 2);
+sum(42, 0);
+console.log("the answer to everything is", sum(42, 0));
+
+const sum2 = (a, b) => a + b;
+
+console.log(`The answer to everything is `${sum(42, 3)});
+*/
+
+/*
+// second practic arrow function example
+function stringLength(str) {
+  console.log(`the length of "${str}" is:`, str.length);
+}
+
+let longestCityNameInTheWorld =
+  "Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu";
+
+stringLength(longestCityNameInTheWorld);
+
+const sl = (str) => console.log(`the length of "${str}" is:`, str.length);
+
+sl(longestCityNameInTheWorld);
+*/
+
+/*
+// third practice example
+function stringLength(str) {
+  let length = str.length;
+  console.log(`the length of "${str}" is:`, length);
+  return str.length;
+}
+
+stringLength("willynilly");
+
+const sl = (str) => {
+  // let length = str.length;
+  // console.log(`the length of "${str}" is:`, length);
+  return str.length;
+};
+
+sl("willynilly");
+
+// trying to do callback function
+let string2 = "funny";
+const func = function (str, a) {
+  console.log(`this ${str}`);
+  console.log(a);
+};
+
+func(string2, sl(string2));
+*/
+
+/*
+// fourth practice arrow function example
+let alerts = [
+  "Hey, you are awesome",
+  "You are so wonderful",
+  "What a marvel you are",
+  "You're so lovely",
+  "You're so sweet that I'd think you're a sweet potato -- and I LOOOOVE POTATOES",
+];
+
+function showAlert(name) {
+  alert(alerts[Math.floor(Math.random() * alerts.length)] + `, ${name}!`);
+}
+
+showAlert("you ball of fluff");
+
+const sa = (str) =>
+  console.log(alerts[Math.floor(Math.random() * alerts.length)] + `, ${str}`);
+
+sa("Jason");
+*/
+
+/*
+// arrow example 5
+function oneTwoThreeRotate() {
+  Array.prototype.slice
+    .call(document.querySelectorAll("div,p,span,img,a,body"))
+    .map(function (tag) {
+      tag.style["transform"] =
+        "rotate(" + (Math.floor(Math.random() * 3) - 1) + "deg)";
+    });
+}
+
+oneTwoThreeRotate();
+
+const rotates = () =>
+  Array.prototype.slice
+    .call(document.querySelectorAll("div,p,span,img,a,body"))
+    .map(function (tag) {
+      tag.style["transform"] =
+        "rotate(" + (Math.floor(Math.random() * 3) - 1) + "deg)";
+    });
+
+rotates();
+*/
+
+/*
+The Collatz Conjecture goes like this:
+
+Take any number n. If n is even, divide it by 2, if n is odd, multiply it by 3 and add 1. Repeat the process indefinitely, and you’ll eventually reach 1.
+
+Given a num variable, can you print all the numbers in num’s Collatz sequence until 1 is reached? Print the numbers space-separated (and each test case on its own line).
+*/
+
+const collatz = (num) => {
+  let str = "";
+  for (let i = 1; i >= num; i++) {
+    if (!(num % 2)) {
+      str.push(num / 2);
+    } else if (!(num % 3)) {
+      str.push(num * 3 + 1);
+    }
+  }
+  console.log(str);
+};
+
+collatz(5);
+
+// reversing string words
 
 let string = "Javascript is best";
-
 /*
 // Hard coded
 const wordsArr = [];
@@ -37,9 +164,6 @@ const reversePhrase = function () {
   for (let i = 0; i < string.length; i++) {
     if (string[i] !== " ") {
       singleWord.push(string[i]);
-      //   if (!singleWord.includes(" ")) {
-      //     wordsArr.push(singleWord.join(""));
-      //   }
     } else if (string[i] == " ") {
       wordsArr.push(singleWord.join(""));
       singleWord = [];
@@ -55,5 +179,11 @@ const reversePhrase = function () {
   const phraseReversed = wordsReversedArr.join(" ");
   return phraseReversed;
 };
+// console.log(reversePhrase());
 
-console.log(reversePhrase(string));
+// more concise way using string methods
+
+function reverse(string) {
+  return string.split(" ").reverse().join(" ");
+}
+// console.log(reverse(string));
